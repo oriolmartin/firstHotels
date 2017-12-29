e7a6eeb702b6012386c290ecaa7ad154ea399db0
oriolmartin <33838878+oriolmartin@users.noreply.github.com>
2017-12-29 19:52:32 +0100

    -

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-      title: 'Hotels',
-      test: ''
-    });
+  res.render('index', { title: 'Hotels' });
 });
 
 module.exports = router;
